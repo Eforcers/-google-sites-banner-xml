075da55135b74bfd5fc7c0245bf247bedd9e7e64
poxstone <poxstone@gmail.com>
2015-11-24 09:27:06 -0500

    git inital commit

---
+++ src/src_index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>eforcers banner for Google sites</title>
+	<meta name="viewport" content="width=device-width , initial-scale=1 ,maximum-scale=1">
+</head>
+<body>
+
+	<link rel="stylesheet" href="./css/banner.css">
+	<div class="wrapper">
+		<?php include('body.html');?>
+	</div>
+	<script src="./js/banner.js"></script>
+	
+</body>
+</html>
